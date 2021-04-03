c9e1f83904ffb42ed8404686b14aff309df68fb7
MikolaPy <patriotsssr19@gmail.com>
2021-04-03 21:34:56 +0300

    add verbose name model Post

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length = 50)
-    content = models.TextField(blank=True,null=True)
-    published = models.DateTimeField(auto_now_add=True,db_index=True)
+    title = models.CharField(max_length = 50,verbose_name='sobject')
+    content = models.TextField(blank=True,null=True,verbose_name='text')
+    published = models.DateTimeField(auto_now_add=True,db_index=True,
+                                    verbose_name='date')
+
+    class Meta:
+        verbose_name_plural = 'Posts'
+        verbose_name = 'Post'
+        ordering = ['-published']
 

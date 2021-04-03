1e7ff079ddb59e8cd440a9f875d172041e84229f
MikolaPy <patriotsssr19@gmail.com>
2021-04-03 21:11:37 +0300

    add main html page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-from django.http import HttpResponse 
+from django.shortcuts import render 
+from .models import Post
 
 def index(request):
-    return HttpResponse("here main page") 
-
+    posts = Post.objects.order_by('-published')
+    context = {'posts':posts}
+    return render(request, 'blog/main.html',context)

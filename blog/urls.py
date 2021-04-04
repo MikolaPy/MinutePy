9ee4ab9f3ace5e0b55e761d17419ff5ac489af09
MikolaPy <patriotsssr19@gmail.com>
2021-04-04 13:17:30 +0300

    index pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import index,by_teg
 
 urlpatterns = [
-    path('<str:teg_name>/',by_teg),
-    path('',index),
+    path('<str:teg_name>/',by_teg,name='by_teg'),
+    path('',index,name='main'),
     ]

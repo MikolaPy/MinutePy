e1cf0db0b2491f9f214534c25fba61cceb4098a2
MikolaPy <patriotsssr19@gmail.com>
2021-04-04 10:14:11 +0300

    add Tag model and AdvUserModel

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post,Teg
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title','content','published')
     list_display_links = ('title','content',)
     search_fields = ('title','content',)
 admin.site.register(Post,PostAdmin)       # admin.site = instance class AdmineSite
-
+admin.site.register(Teg)
 
 
--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+" for register and setting display fields model need change options in blog/admin.py "
+
+class AdvUser(models.Model):
+    is_activated = models.BooleanField(default=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
 
 
 class Post(models.Model):
-    " for setting display fields model need change par in blog/admin.py "
-
     title = models.CharField(max_length = 50,verbose_name='sobject')
     content = models.TextField(blank=True,null=True,verbose_name='text')
-    published = models.DateTimeField(auto_now_add=True,db_index=True,
-                                    verbose_name='date')
+    published = models.DateTimeField(auto_now_add=True,db_index=True,verbose_name='date')
+    tags = models.ManyToManyField(
+                                    'Teg',
+                                    related_name = 'posts', #access to records , instead Post_set 
+                                    )
 
     class Meta:
         verbose_name_plural = 'Posts'
         verbose_name = 'Post'
         ordering = ['-published']
-
+        
+    def __str__(self):
+        return self.title
 
 class Teg(models.Model):
     name = models.CharField(max_length= 40,db_index = True ,
@@ -23,3 +34,5 @@ class Teg(models.Model):
         verbose_name_plural = 'tegs'
         verbose_name = 'teg'
         ordering = ['name']
+    def __str__(self):
+        return self.name

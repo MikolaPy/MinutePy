92f7d5418bbc0bd1feddf618008028eecf7255ac
MikolaPy <patriotsssr19@gmail.com>
2021-04-06 10:58:29 +0300

    add post list page and PostList view class , delete view index

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('create/',PostCreateView.as_view(),name='create'),
-    path('',index,name='main'),
-    path('<str:teg_pk>/',PostByTegView.as_view(),name='by_teg'),
+    path('',AllPostView.as_view(),name='main'),
+    path('<str:teg_name>/',PostByTegView.as_view(),name='by_teg'),
     path('post/<int:pk>',PostDetailView.as_view(),
          name='postdetail'),
     ]
--- blog/views.py
+++ blog/views.py
@@ -8,24 +8,39 @@ from django.views.generic.list import ListView
 from .models import *
 from .forms import PostForm
 
-
+'''
 def index(request):
     posts = Post.objects.all()
     tegs = Teg.objects.all()
     context = {'posts':posts,'tegs':tegs}
     return render(request, 'blog/main.html',context)
+'''
+
+
+class AllPostView(ListView):
+    model = Post    #all posts in object_list attr , template post_list.html
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tegs'] = Teg.objects.all()
+        context['posts'] = context['object_list']
+        return context
+
+
 
 
 class PostByTegView(ListView):
-    template_name = 'blog/main.html'
+    template_name = 'blog/posts_by_teg.html'
     context_object_name = 'posts'
     def get_queryset(self):
-        return Post.objects.filter(tegs__name=self.kwargs['teg_pk'])
+        return Post.objects.filter(tegs__name=self.kwargs['teg_name'])
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['teg'] = Teg.objects.get(name = self.kwargs['teg_name'])
+        return context
 
 
 class PostDetailView(DetailView):
     model = Post
-
 
 
 class PostCreateView(CreateView):

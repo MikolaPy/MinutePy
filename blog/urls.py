db5bdada31b08086d8211486e28c1236c18cde49
MikolaPy <patriotsssr19@gmail.com>
2021-04-06 12:58:11 +0300

    add edit page and view class UpdateView adrees 'viewpost/edit' , html 'post_form.html'

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('create/',PostCreateView.as_view(),name='create'),
     path('',AllPostView.as_view(),name='main'),
     path('<str:teg_name>/',PostByTegView.as_view(),name='by_teg'),
-    path('post/<int:pk>',PostDetailView.as_view(),
-         name='postdetail'),
+    path('post/<int:pk>/edit/',PostEdit.as_view(),name='editpost'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name='postdetail'),
     ]
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import *
 from .forms import PostForm
 
-'''
+"""
 def index(request):
     posts = Post.objects.all()
     tegs = Teg.objects.all()
     context = {'posts':posts,'tegs':tegs}
     return render(request, 'blog/main.html',context)
-'''
-
+"""
 
 class AllPostView(ListView):
     model = Post    #all posts in object_list attr , template post_list.html
@@ -24,8 +23,6 @@ class AllPostView(ListView):
         context['tegs'] = Teg.objects.all()
         context['posts'] = context['object_list']
         return context
-
-
 
 
 class PostByTegView(ListView):
@@ -48,5 +45,10 @@ class PostCreateView(CreateView):
     form_class = PostForm
     success_url = reverse_lazy('main')
 
+class PostEdit(UpdateView):
+    teplate_name = 'blog/post_edit.html'
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('main')
 
 

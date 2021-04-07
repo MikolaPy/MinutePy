f959b9f161d7bc6f0a5babbc1d5540a932a0958f
MikolaPy <patriotsssr19@gmail.com>
2021-04-07 14:07:48 +0300

    form post change validator

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,18 @@
 from django import forms
-
+from django.core.exceptions import ValidationError 
 from .models import *
 
 class PostForm(forms.ModelForm):
     title = forms.CharField(label='Subject')
-    content = forms.CharField(label='bodypost',widget=forms.widgets.Textarea())
+    content = forms.CharField(label='bodypost',widget=forms.widgets.Textarea(),required=False)
     tegs = forms.ModelMultipleChoiceField(queryset=Teg.objects.all(),label='teg',widget=forms.widgets.CheckboxSelectMultiple(attrs={'size':10}))
+    def clean(self):
+        super().clean()
+        errors = {}
+        if not self.cleaned_data['content']:
+            errors['content'] = ValidationError('WTF')
+        if errors:
+            raise ValidationError(errors)
 
     class Meta:
         model = Post

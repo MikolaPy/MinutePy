77dab82ff4c74a28766406a66ce6fb1f966c1b20
MikolaPy <patriotsssr19@gmail.com>
2021-04-10 07:52:35 +0300

    new basic page and css  + bootstrap

--- MinutePy/urls.py
+++ MinutePy/urls.py
@@ -3,9 +3,16 @@ from django.urls import path,include
 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.views import serve
+from django.views.decorators.cache import never_cache
+
 urlpatterns = [
     path('',include('blog.urls')),     #main apps url 
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>',never_cache(serve)))

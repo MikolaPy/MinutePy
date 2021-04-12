7a622f6b72d4ddc5548f5637de87925987123e98
MikolaPy <patriotsssr19@gmail.com>
2021-04-12 22:28:11 +0300

    pathed manager context function , add variable all , she save get parameters page and key

--- blog/middlewares.py
+++ blog/middlewares.py
@@ -3,5 +3,18 @@ from .models import *
 def post_context_processor(request):
     context = {}
     context['markers'] = Marker.objects.all()
+    context['key'] = ''
+    context['all'] = ''
+    if 'key' in request.GET:
+        key = request.GET['key']
+        if key:
+            context['key'] = '?key='+key
+            context['all'] = context['key']
+    if 'page' in request.GET:
+        page = request.GET['page']
+        if page != '1':
+            if context['all']:
+                context['all'] += '?page=' + page
+            else:
+                context['all'] = '?page=' + page
     return context
-
